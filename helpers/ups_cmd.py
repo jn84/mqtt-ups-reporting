d459ae2b13b47b8a8e91edc3f74dd0726d80284b
Jeremy <jmnims@gmail.com>
2019-04-08 18:23:51 -0400

    Basic code for UPSState Basic code for UPSCommand

--- helpers/ups_cmd.py
+++ helpers/ups_cmd.py
@@ -1,4 +1,27 @@
 # Handle incoming UPS commands
 
+from pynut2 import nut2 as nut
+
+
 class UPSCommand:
-    blank_var = None
+    _ups_client = None
+    _ups_name = None
+
+    _ls_ups_cmds = list()
+
+
+    def __init__(self, ups_name, username, password):
+        self._ups_client = nut.PyNUTClient(login=username, password=password)
+        self._ups_name = ups_name
+
+        # Initialize available commands list
+        for ups_var in self._ups_client.list_commands(self._ups_name):
+            self._ls_ups_cmds.append(ups_var)
+
+    # Update state dictionary with latest data
+    def all_commands(self):
+        for cmd in self._ls_ups_cmds:
+            yield cmd
+
+    def run_command(self, command, params=""):
+        self._ups_client.run_command(command + " " + params)
--- helpers/ups_state.py
+++ helpers/ups_state.py
@@ -1,19 +1,37 @@
 # Monitor UPS state
 
+from pynut2 import nut2 as nut
+
 class UPSState:
+    _ups_client = None
 
-    _state_dict = None
+    _ups_name = None
 
-    _poll_interval =
+    _ls_ups_vars = list()
+    _dict_states = dict()
 
     # Value List: [ current value (str/int/float), has updated (Bool) ]
 
-    def __init__(self, init_vars):
-        self._dict_base = dict()
-        for var in init_vars:
-            self._dict_base[var] = [None, True]
+    def __init__(self, ups_name):
+        self._ups_client = nut.PyNUTClient()
+        self._ups_name = ups_name
 
-    async def loop_forever(self):
-        # do loopy stuff
+        # Initialize dictionary keys with all available states
+        for ups_var in self._ups_client.list_vars(self._ups_name):
+            self._ls_ups_vars.append(ups_var)
+            self._dict_states[ups_var] = [None, False]
 
+    # Update state dictionary with latest data
+    def _update_states(self):
+        for ups_var in self._ls_ups_vars:
+            new_state = self._ups_client.get_var(self._ups_name, ups_var)
+            if self._dict_states[ups_var][0] != new_state:
+                self._dict_states[ups_var] = [new_state, True]
+            else:
+                self._dict_states[ups_var][1] = False
 
+    def get_state_data(self):
+        self._update_states()
+        for key, value in self._dict_states.items():
+            if value[1]:
+                yield [key, value[0]]
